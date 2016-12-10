0a641e0a1e79c1df03ad68e3d02212255a6f58f7
Avi <Avi@Avi-Lp.home>
2016-12-10 13:21:22 +0200

    Income web client proxy

---
+++ WebContent/services/proxy/incomeProxy.service.js
@@ -0,0 +1,33 @@
+angular.module("Coupon")
+    .service('incomeProxy', function ($http) {
+        var adminBaseUrl = "coupon/admin";
+        var CompanyBaseUrl = "coupon/admin";
+
+        // can be called only from a COMPANY account
+        this.getConpanyPanyments = function () {
+            return $http.get(CompanyBaseUrl + "/getPayments");
+        };
+
+        // can be called only from a ADMIN account
+        this.getAllIncome = function () {
+            return $http.get(adminBaseUrl + "/getAllIncome");
+        };
+
+        this.getIncomeByCompany = function (id) {
+            return $http({
+                method: 'POST',
+                url: adminBaseUrl + "/getIncomeByCompany",
+                headers: {'Content-Type': 'text/plain'},
+                data: id
+            });
+        };
+
+        this.getIncomeByCustomer = function (id) {
+            return $http({
+                method: 'POST',
+                url: adminBaseUrl + "/getIncomeByCustomer",
+                headers: {'Content-Type': 'text/plain'},
+                data: id
+            });
+        };
+    });
